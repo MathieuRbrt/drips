070b23a5777542af2d3d35a47ec57e7d00008c46
Mathieu Robert <mathieu.robert14@gmail.com>
2014-05-15 16:26:14 +0100

    Updated profile pages

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   get "create", to: 'posts#new', as: :create_post
   root to: 'posts#index'
 
+  get '/:id', to: 'profiles#show'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- test/controllers/profiles_controller_test.rb
+++ test/controllers/profiles_controller_test.rb
@@ -2,8 +2,26 @@ require 'test_helper'
 
 class ProfilesControllerTest < ActionController::TestCase
   test "should get show" do
-    get :show
+    get :show, id: users(:mathieu).profile_name
     assert_response :success
+    assert_template 'profiles/show'
   end
 
+  	test "should render a 404 on profile not found" do
+		get :show, id: "doesn't exist"
+    	assert_response :not_found
+	end
+
+  	test "that variables are assigned on successful profile viewing" do
+		get :show, id: users(:mathieu).profile_name
+    	assert assigns(:user)
+    	assert_not_empty assigns(:posts)
+	end
+
+  	test "only shows the correct's user's posts" do
+		get :show, id: users(:mathieu).profile_name
+		assigns(:posts).each do |post|
+			assert_equal users(:mathieu), post.user
+		end
+	end
 end
